80f5e100d73a5c30cf963bfc3a8a583ac9241554
bartlomiej.kierys <bskierys@gmail.com>
2018-03-04 10:53:11 +0100

    Remove unused boilerplate code

--- app/src/main/kotlin/pl/ipebk/setsolver/ApplicationComponent.kt
+++ app/src/main/kotlin/pl/ipebk/setsolver/ApplicationComponent.kt
@@ -2,29 +2,21 @@ package pl.ipebk.setsolver
 
 import dagger.Component
 import pl.ipebk.setsolver.data.network.NetworkModule
-import pl.ipebk.setsolver.data.remote.ApiModule
-import pl.ipebk.setsolver.ui.detail.DetailComponent
-import pl.ipebk.setsolver.ui.detail.DetailModule
-import pl.ipebk.setsolver.ui.list.ListComponent
-import pl.ipebk.setsolver.ui.list.ListModule
 import pl.ipebk.setsolver.ui.solver.SolverComponent
 import pl.ipebk.setsolver.ui.solver.SolverModule
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = arrayOf(
-        ApplicationModule::class,
-        NetworkModule::class,
-        ApiModule::class
+  ApplicationModule::class,
+  NetworkModule::class
 ))
 interface ApplicationComponent {
 
-    // Injectors
-    fun injectTo(app: KotlinBoilerplateApp)
+  // Injectors
+  fun injectTo(app: KotlinBoilerplateApp)
 
-    // Submodule methods
-    // Every screen is its own submodule of the graph and must be added here.
-    fun plus(module: ListModule): ListComponent
-    fun plus(module: DetailModule): DetailComponent
-    fun plus(module: SolverModule) : SolverComponent
+  // Submodule methods
+  // Every screen is its own submodule of the graph and must be added here.
+  fun plus(module: SolverModule): SolverComponent
 }
